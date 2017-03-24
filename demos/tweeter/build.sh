a30ff590bd3d33bf1acf8b1089498262999983e0
Alistair A. Israel <aisrael@gmail.com>
2017-03-24 11:41:55 +0800

    Docker all the things

---
+++ demos/tweeter/build.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+while [[ $# > 0 ]]
+do
+key="$1"
+case $key in
+    -w|--working-tree)
+    USE_WORKING_TREE=1
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+# Exit on error (e.g., "Not a git repository")
+set -e
+TOP_LEVEL=$(git rev-parse --show-toplevel)
+set +e
+
+REPO_NAME='tweeter'
+
+CURRENT_BRANCH=$(git symbolic-ref --short HEAD)
+
+if [[ -z "$TMP" ]]; then
+  TMP=$(mktemp -d)
+else
+  mkdir -p "$TMP"
+fi
+
+
+if [ -z ${USE_WORKING_TREE+x} ]; then
+  echo git checkout-index --prefix="$TMP/" -a
+  git checkout-index --prefix="$TMP/" -a
+else
+  echo rsync -av . "$TMP" --exclude .git --exclude node_modules --exclude tmp
+  rsync -av . "$TMP" --exclude .git --exclude node_modules --exclude tmp
+fi
+
+
+echo git submodule foreach --quiet 'git checkout-index --prefix="'$TMP'/$path/" -a'
+git submodule foreach --quiet 'git checkout-index --prefix="'$TMP'/$path/" -a'
+
+if ! [ -z ${USE_WORKING_TREE+x} ]; then
+  echo rm -rf "$TMP/.git"
+  rm -rf "$TMP/.git"
+fi
+
+if [ -z ${USE_WORKING_TREE+x} ] && [ ! "$CURRENT_BRANCH" = "develop" ]; then
+  IMAGE_TAG=$(basename "$CURRENT_BRANCH")
+
+  echo docker build -t "aisrael/$REPO_NAME:$IMAGE_TAG" "$TMP"
+  docker build -t "aisrael/$REPO_NAME:$IMAGE_TAG" "$TMP"
+else
+  echo docker build -t "aisrael/$REPO_NAME" "$TMP"
+  docker build -t "aisrael/$REPO_NAME" "$TMP"
+fi
